69396e54646c38b81d338cd0e04b24afcebb8a6c
7osain <h.k.h.e.2000@gmail.com>
2024-08-15 10:44:28 +0300

    Setup api resources for all models

---
+++ app/Http/Resources/ListResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'user' => $this->whenLoaded('user', fn() => UserResource::make($this->user)),
+            'tasksCount' => $this->whenCounted('tasks'),
+            'completedTasksCount' => $this->whenCounted('completedTasks'),
+            'uncompletedTasksCount' => $this->whenCounted('uncompletedTasks'),
+        ];
+    }
+}
---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'priority' => $this->priority,
+            'completed' => $this->completed,
+            'section' => $this->whenLoaded('section', fn() => SectionResource::make($this->section)),
+            'taskItemsCount' => $this->whenCounted('items'),
+            'completedTaskItemsCount' => $this->whenCounted('completedTaskItems'),
+            'uncompletedTaskItemsCount' => $this->whenCounted('uncompletedTaskItems'),
+        ];
+    }
+}
